27020dd253cb5e5ac58355df7a4acd48a915832b
DaanSterk <daan-sterk@hotmail.com>
2016-02-20 17:47:48 +0100

    Soundboard working (basic)

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -8,8 +8,28 @@ class SoundsController < ApplicationController
 
   def create
     @board = Board.find(Rails.cache.read("board"))
-    @board.sounds.create(sound_params) # How to fix????
+    @model = @board.sounds.create(sound_params)
+    @model.xPos = Rails.cache.read("xPos")
+    @model.yPos = Rails.cache.read("yPos")
+    @model.save
     if @board.save!
+      return redirect_to board_path(Rails.cache.read("board"))
+    end
+  end
+
+  def edit
+    Rails.cache.write("board", params[:board])
+    Rails.cache.write("xPos", params[:xPos])
+    Rails.cache.write("yPos", params[:yPos])
+    @sound = Sound.find(params[:id])
+  end
+
+  def update
+    @sound = Sound.find(params[:id])
+    @sound.update_attributes(sound_params)
+    @sound.xPos = Rails.cache.read("xPos")
+    @sound.yPos = Rails.cache.read("yPos")
+    if @sound.save
       return redirect_to board_path(Rails.cache.read("board"))
     end
   end
